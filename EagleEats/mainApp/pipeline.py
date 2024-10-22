808bb3440d788c27b266ad7acccb6ba66c9fbe91
Andy <azheng2639@gmail.com>
2024-10-22 00:26:36 -0400

    cleared updates from requirements, updated login button

--- EagleEats/mainApp/pipeline.py
+++ EagleEats/mainApp/pipeline.py
@@ -1,7 +1,5 @@
 def check_first_login(backend, user, response, *args, **kwargs):
-    # Check if it's the first time the user is logging in
     if user and not user.is_staff and not user.last_login:
-        # If it's the first time, store a flag in the session
         backend.strategy.session_set('is_first_login', True)
     else:
         backend.strategy.session_set('is_first_login', False)
