0bd0f1ab64a2af310964581287be244d6355e20f
Andy <zhengno@bc.edu>
2024-12-02 18:40:47 -0500

    updates to build script

--- EagleEats/build.sh
+++ EagleEats/build.sh
@@ -10,10 +10,8 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 
-python manage.py delete_all_campaigns
-python manage.py create_test_campaigns
+
+python manage.py wipe_database
+
 
 python manage.py collectstatic --no-input
-
-
-cd EagleEats
