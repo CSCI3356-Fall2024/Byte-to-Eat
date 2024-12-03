bbbc9541643ae134d4f76b564b0af3fcf3093fc6
Andy <zhengno@bc.edu>
2024-12-03 13:30:12 -0500

    undo changes to static

--- EagleEats/EagleEats/settings.py
+++ EagleEats/EagleEats/settings.py
@@ -212,13 +212,13 @@ import os
 
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
-# STATIC_URL = '/static/'
-# # This production code might break development mode, so we check whether we're in DEBUG mode
-# if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-#     # and renames the files with unique names for each version to support long-term caching
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ##default weekly group challenge points
 GROUP_CHALLENGE_POINTS = 500
