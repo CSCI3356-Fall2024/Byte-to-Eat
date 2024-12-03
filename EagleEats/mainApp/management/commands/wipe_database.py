684c6c6d8409bf3bce1543aed95c10e92f379f00
Andy <zhengno@bc.edu>
2024-12-03 13:51:20 -0500

    update database wiping script

--- EagleEats/mainApp/management/commands/wipe_database.py
+++ EagleEats/mainApp/management/commands/wipe_database.py
@@ -1,3 +1,5 @@
+from mainApp.models import Profile, Group, Campaign
+from django.contrib.auth.models import User
 from django.db import connection
 from django.core.management.base import BaseCommand
 
@@ -5,6 +7,13 @@ class Command(BaseCommand):
     help = "wipe database"
 
     def handle(self, *args, **kwargs):
+        Profile.objects.all().delete()
+        Group.objects.all().delete()
+        User.objects.all().delete()
+        Campaign.objects.all().delete()
+
+
         with connection.cursor() as cursor:
             cursor.execute("DROP SCHEMA public CASCADE;")
             cursor.execute("CREATE SCHEMA public;")
+        self.stdout.write(self.style.SUCCESS("Successfully wiped database"))
