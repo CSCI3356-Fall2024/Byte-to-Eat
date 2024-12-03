90abeedfb171d5a1362a9a4d323bf608378415df
Andy <zhengno@bc.edu>
2024-12-03 10:39:00 -0500

    rollout test campaigns

--- EagleEats/build.sh
+++ EagleEats/build.sh
@@ -10,6 +10,8 @@ pip install -r requirements.txt
 python manage.py wipe_database
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
+
+python manage.py create_test_campaigns
 python manage.py create_test_users
 
 python manage.py collectstatic --no-input
