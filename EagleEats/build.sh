95fc8de7be7f028aec658516fa2224741b562993
Andy <zhengno@bc.edu>
2024-12-03 13:56:42 -0500

    recreate test environment

--- EagleEats/build.sh
+++ EagleEats/build.sh
@@ -9,6 +9,8 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 
+python manage.py create_test_users
+python manage.py create_test_campaigns
 # Collect static files
 
 python manage.py collectstatic --no-input
