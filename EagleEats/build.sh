d8798c2663a7584fbe49a39c10ed3c5223bf5e44
Andy <zhengno@bc.edu>
2024-12-03 09:45:09 -0500

    restructure build script

--- EagleEats/build.sh
+++ EagleEats/build.sh
@@ -5,12 +5,14 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static asset files
+# Run database migrations
 python manage.py makemigrations
 python manage.py migrate
 
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run custom management commands
 python manage.py wipe_database
 python manage.py create_test_users
 python manage.py create_test_campaigns
-
-python manage.py collectstatic --no-input
