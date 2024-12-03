eb8da7dd46fd035459a832fdf393bda5ed43aa2b
Andy <zhengno@bc.edu>
2024-12-03 02:49:13 -0500

    fixed admin registration typo

--- EagleEats/mainApp/admin.py
+++ EagleEats/mainApp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import Group, Profile, Campaign
+from .models import Group, Profile, Campaign
 
 admin.site.register(Group)
 admin.site.register(Profile)
