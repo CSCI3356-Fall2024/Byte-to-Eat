2c0ecf502c3a81bba6f4f992f0d729832947b2b0
Andy <zhengno@bc.edu>
2024-12-03 02:44:51 -0500

    added views to admin site

--- EagleEats/mainApp/admin.py
+++ EagleEats/mainApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from models import Group, Profile, Campaign
+
+admin.site.register(Group)
+admin.site.register(Profile)
+admin.site.register(Campaign)
 # Register your models here.
