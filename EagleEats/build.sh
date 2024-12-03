b8a725a96311e9549911224243d216a3ff41bdd1
Andy <zhengno@bc.edu>
2024-12-02 21:49:07 -0500

    intermediate updates to build script

--- EagleEats/build.sh
+++ EagleEats/build.sh
@@ -7,7 +7,9 @@ pip install -r requirements.txt
 
 # Convert static asset files
 
+python manage.py wipe_database
 python manage.py makemigrations
 python manage.py migrate
+python manage.py create_test_users
 
 python manage.py collectstatic --no-input
